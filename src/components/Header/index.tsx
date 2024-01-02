175636ad9c4b19706325f291e4d8a16e4e3f3b68
Rodrigo Silva <rdg.hora1@gmail.com>
2024-01-02 13:06:29 -0300

    modified:   src/components/Header/index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
       <img
         src={logo}
         alt=""
-        className="3xl:ml-[900px] mb-5 ml-32 mt-12 sm:ml-72 md:ml-[350px] lg:ml-[470px] xl:ml-[600px] 2xl:ml-[730px]"
+        className="mb-5 ml-32 mt-12 sm:ml-72 md:ml-[350px] lg:ml-[470px] xl:ml-[700px] 2xl:ml-[730px] 3xl:ml-[900px]"
       />
       <h1 className="mb-7 ml-2 text-center text-3xl font-bold GalaxyS8:text-2xl">
         Não sabe o que assistir ?
